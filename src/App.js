b9e530a08173565139e7ce406add31f7a26da581
AvresCode <servas230@gmail.com>
2023-07-04 19:48:27 +0200

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
 function App() {
   return (
-    <div>
-      <h2>Home</h2>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/myprofile" element={<Profile />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
