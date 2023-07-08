7158bf5ab25bbbd737f04d949657aefea2fffa17
AvresCode <servas230@gmail.com>
2023-07-08 21:53:57 +0200

    add user registered with google to the database

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,7 +1,8 @@
 import { FcGoogle } from 'react-icons/fc';
 import { toast } from 'react-toastify';
-import { auth } from '../firebase';
+import { auth, db } from '../firebase';
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
 
 export default function OAuth() {
   async function onGoogleClick() {
@@ -9,6 +10,22 @@ export default function OAuth() {
       const provider = new GoogleAuthProvider();
       const response = await signInWithPopup(auth, provider);
       const user = response.user;
+
+      //check the user
+      //make a reference to the user
+      const docRef = doc(db, 'users', user.uid);
+
+      //check if the user is already available
+      const docSnap = await getDoc(docRef);
+
+      //if it doesn't exist, add it to the database
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
     } catch (error) {
       toast.error('Could not authorize with Google');
     }
