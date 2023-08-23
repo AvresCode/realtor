6524a5ed23b2de1b603d8c1df53d4199f9d5c793
AvresCode <servas230@gmail.com>
2023-08-23 13:06:39 +0200

    add renting section to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,7 @@ import ListingItem from '../components/ListingItem';
 import { Link } from 'react-router-dom';
 
 export default function Home() {
+  //Get offered listings
   const [listingsOffers, setListingsOffers] = useState(null);
 
   useEffect(() => {
@@ -39,13 +40,40 @@ export default function Home() {
     fetchListings();
   }, []);
 
+  //Get rent listings
+  const [rentListings, setRentListings] = useState(null);
+
+  useEffect(() => {
+    async function fetchListings() {
+      try {
+        const listingRef = collection(db, 'listings');
+        const q = query(
+          listingRef,
+          where('type', '==', 'rent'),
+          orderBy('timestamp', 'desc'),
+          limit(4)
+        );
+        const querySnap = await getDocs(q);
+        const listings = [];
+        querySnap.forEach((doc) => {
+          return listings.push({ id: doc.id, data: doc.data() });
+        });
+        setRentListings(listings);
+        console.log('RENT', listings);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchListings();
+  }, []);
+
   return (
     <>
       <Slider />
       <div className="max-w-6xl mx-auto pt-4 space-y-6 my-10">
         {listingsOffers && listingsOffers.length > 0 && (
           <div className="m-2 mb-6">
-            <h2 className="py-5 text-2xl mt-6 font-semibold">Recent offers</h2>
+            <h2 className="py-5 text-2xl mt-6 font-semibold">Recent Offers</h2>
             <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
               {listingsOffers.map((listing) => (
                 <ListingItem
@@ -57,7 +85,28 @@ export default function Home() {
             </ul>{' '}
             <Link to="/offers">
               <p className="p-2  text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
-                Show more offers
+                Discover More Deals
+              </p>
+            </Link>
+          </div>
+        )}{' '}
+        {rentListings && rentListings.length > 0 && (
+          <div className="m-2 mb-6">
+            <h2 className="py-5 text-2xl mt-6 font-semibold">
+              Renting Opportunities
+            </h2>
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+              {rentListings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                />
+              ))}
+            </ul>{' '}
+            <Link to="/category/rent">
+              <p className="p-2  text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
+                Explore More Rental Spaces
               </p>
             </Link>
           </div>
