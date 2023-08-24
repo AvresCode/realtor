e6e39d2f2bcfcfb17ef15cb10755aeca79df9159
AvresCode <servas230@gmail.com>
2023-08-24 19:57:41 +0200

    add offer page data and display

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,3 +1,58 @@
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { db } from '../firebase';
+import Spinner from '../components/Spinner';
+import ListingItem from '../components/ListingItem';
+
 export default function Offers() {
-  return <div>offer</div>;
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    async function fetchListings() {
+      const listingRef = collection(db, 'listings');
+      const q = query(
+        listingRef,
+        where('offer', '==', true),
+        orderBy('timestamp', 'desc'),
+        limit(4)
+      );
+
+      const docSnap = await getDocs(q);
+      const listings = [];
+      docSnap.forEach((doc) => {
+        return listings.push({ id: doc.id, data: doc.data() });
+      });
+      setListings(listings);
+      setLoading(false);
+      console.log('listings', listings);
+    }
+    fetchListings();
+  }, []);
+  return (
+    <div className="max-w-6xl mx-auto px-3">
+      <h1 className="text-3xl text-center mt-6 font-bold mb-6">Offers</h1>
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5">
+          {listings.map((listing) => (
+            <ListingItem
+              key={listing.id}
+              id={listing.id}
+              listing={listing.data}
+            />
+          ))}
+        </ul>
+      ) : (
+        <p>No Listings with Offers Found</p>
+      )}
+    </div>
+  );
 }
